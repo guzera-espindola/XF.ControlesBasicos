5789e816bc4df407a2775b8721022d1d85f88c25
Gustavo <Gustavo@DESKTOP-U2BAFKE>
2019-05-20 22:19:40 -0300

    Alterações aula 1

--- XF.ControlesBasicos/XF.ControlesBasicos/MainPage.xaml.cs
+++ XF.ControlesBasicos/XF.ControlesBasicos/MainPage.xaml.cs
@@ -9,9 +9,37 @@ namespace XF.ControlesBasicos
 {
     public partial class MainPage : ContentPage
     {
+        private Configuracao _vmConfig;
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        void ButtonEnviarClicked(object sender, EventArgs e)
+        {
+            if (_vmConfig == null)
+                _vmConfig = new Configuracao();
+
+            if (_vmConfig.ReceberEmail)
+            {
+                DisplayAlert("Mensagem"
+                    , "E-mail enviado para" + _vmConfig.Email, "OK");
+            }
+            else
+            {
+                DisplayAlert("Mensagem"
+                    , "E-mail não autorizado", "OK");
+            }
+            
+        }
+
+        void ButtonConfiguracaoClicked(object sender, EventArgs e)
+        {
+
+            if (_vmConfig == null)
+                _vmConfig = new Configuracao();
+
+            Navigation.PushAsync(new ConfigPage() { BindingContext = _vmConfig });
         }
     }
 }
